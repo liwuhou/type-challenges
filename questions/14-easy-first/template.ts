cacd0790e70aa76865dd2e8b464138e7d1d4bdab
阿五 <hugewilliam@foxmail.com>
2022-02-14 10:15:53 +0800

    solve: 14-easy-first

--- questions/14-easy-first/template.ts
+++ questions/14-easy-first/template.ts
@@ -1,1 +1,4 @@
-type First<T extends any[]> = any
+// type First<T extends any[]> = T extends [] ? never : T[0]
+// type First<T extends any[]> = T extends [] ? never : T[0]
+// type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
+type First<T extends any[]> = T extends [infer First, ...infer _Rest] ? First : never
