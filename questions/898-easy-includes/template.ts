695f0af3a4fbe6f9896a665e87b67f8ffbfa3ecf
阿五 <hugewilliam@foxmail.com>
2022-02-23 08:52:25 +0800

    solve: 898

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any;
+import { Equal, Expect } from '@type-challenges/utils'
+
+export type Includes<T extends readonly any[], U> =
+  T extends [infer First, ...infer Rest]
+    ? [First, U] extends [U, First]
+      ? Equal<First, U>
+      : Includes<Rest, U>
+    : false
