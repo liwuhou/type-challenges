04b14d0713eefaa99d9d1a5bf80c1ffce01463a2
阿五 <hugewilliam@foxmail.com>
2022-03-08 08:55:02 +0800

    solve: 62

--- questions/62-medium-type-lookup/template.ts
+++ questions/62-medium-type-lookup/template.ts
@@ -1,1 +1,14 @@
-type LookUp<U, T> = any
+interface Thing {
+  type: string
+  [key: string]: any
+}
+
+// type LookUp<U extends Thing, T extends string> =
+//   U extends U
+//     ? U['type'] extends T
+//       ? U
+//       : never
+//     : never
+
+// better
+type LookUp<U extends {type: string}, T extends string> = U extends {type: T} ? U : never
