80fea14a432d548dacf2adcfbef6ecf16488fe08
阿五 <hugewilliam@foxmail.com>
2022-03-13 23:49:51 +0800

    solve: 459

--- questions/459-medium-flatten/template.ts
+++ questions/459-medium-flatten/template.ts
@@ -1,1 +1,13 @@
-type Flatten = any
+type Flatten<T extends unknown[], Result extends unknown[] = []> =
+  T extends [first: infer First, ...rest: infer Rest]
+    ? First extends any[]
+      ? Flatten<Rest, [...Result, ...Flatten<First>]>
+      : Flatten<Rest, [...Result, First]>
+    : Result
+
+type FlattenBetter<T extends unknown[]> =
+  T extends [infer First, ...infer Rest]
+    ? First extends unknown[]
+      ? [...FlattenBetter<First>, ...FlattenBetter<Rest>]
+      : [First, ...FlattenBetter<Rest>]
+    : T
