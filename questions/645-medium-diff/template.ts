70616b5d8da329f7222440b1631e7aaa9af791c0
阿五 <hugewilliam@foxmail.com>
2022-03-19 18:27:05 +0800

    solve: 645

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,1 +1,8 @@
-type Diff<O, O1> = any
+type Diff<O extends Record<string, any>, O1 extends Record<string, any>> = {
+  [Key in keyof O | keyof O1 as Key extends keyof O
+    ? Key extends keyof O1
+      ? never
+      : Key
+    : Key
+  ]: Key extends keyof O ? O[Key] : Key extends keyof O1 ? O1[Key] : never
+}
