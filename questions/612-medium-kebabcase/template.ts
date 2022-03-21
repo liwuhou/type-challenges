6911a9bc1fcc6443e8e2a3c048a02fa5e0182bef
阿五 <hugewilliam@foxmail.com>
2022-03-21 14:25:55 +0800

    solve: 612

--- questions/612-medium-kebabcase/template.ts
+++ questions/612-medium-kebabcase/template.ts
@@ -1,1 +1,15 @@
-type KebabCase<S> = any;
+// type KebabCase<S extends string, isFrist extends boolean = true> =
+//   S extends `${infer First}${infer Rest}`
+//     ? First extends Lowercase<First>
+//       ? `${First}${KebabCase<Rest, false>}`
+//       : `${isFrist extends true ? '' : '-'}${Lowercase<First>}${KebabCase<Rest, false>}`
+//     : S
+
+type KebabCase<S extends string> =
+  S extends `${infer First}${infer Rest}`
+    ? `${Lowercase<First>}${
+      Rest extends Uncapitalize<Rest>
+        ? KebabCase<Rest>
+        : `-${KebabCase<Capitalize<Rest>>}`
+    }`
+    : S
