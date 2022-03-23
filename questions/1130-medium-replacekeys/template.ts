aa99cebdc64b7cd26d33da49873625ae501b00b3
阿五 <hugewilliam@foxmail.com>
2022-03-23 16:17:52 +0800

    solve:1130

--- questions/1130-medium-replacekeys/template.ts
+++ questions/1130-medium-replacekeys/template.ts
@@ -1,1 +1,7 @@
-type ReplaceKeys<U, T, Y> = any
+type ReplaceKeys<
+  U extends Record<string, any>,
+  T extends string,
+  Y extends Record<string, any>
+> = U extends U ? {
+  [Key in keyof U]: Key extends T ? Key extends keyof Y ? Y[Key] : never : U[Key]
+} : never
