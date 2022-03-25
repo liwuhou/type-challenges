9827bbc9d3dfa987c6063cd612cd9481d4676f1a
阿五 <hugewilliam@foxmail.com>
2022-03-25 21:19:48 +0800

    solve: 1978

--- questions/1978-medium-percentage-parser/template.ts
+++ questions/1978-medium-percentage-parser/template.ts
@@ -1,1 +1,12 @@
-type PercentageParser<A extends string> = any
+type PercentNumParser<S extends string> = 
+  S extends `${infer PNum}%`
+    ? [PNum, '%']
+    : [S, '']
+
+type PercentageParser<A extends string> = 
+  A extends `-${infer Percent}`
+    ? ['-', ...PercentNumParser<Percent>]
+    : A extends `+${infer Percent}`
+      ? ['+', ...PercentNumParser<Percent>]
+      : ['', ...PercentNumParser<A>]
+
