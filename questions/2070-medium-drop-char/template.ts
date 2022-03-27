9130c51a090e6ee62fc17775f150152a8527f6f0
阿五 <hugewilliam@foxmail.com>
2022-03-27 23:46:24 +0800

    solve: 2070

--- questions/2070-medium-drop-char/template.ts
+++ questions/2070-medium-drop-char/template.ts
@@ -1,1 +1,13 @@
-type DropChar<S, C> = any
+
+
+type DropChar<S extends string, C extends string, Result extends string = ''> =
+  S extends `${infer F}${infer L}`
+    ? C extends ''
+      ? F extends ' '
+        ? DropChar<L, C, Result>
+        : DropChar<L, C, `${Result}${F}`>
+      : F extends C
+        ? DropChar<L, C, Result>
+        : DropChar<L, C, `${Result}${F}`>
+    : Result
+
