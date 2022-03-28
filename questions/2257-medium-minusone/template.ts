5faaaeea083575e9e26e7f7edf613ae210c8ee6c
阿五 <hugewilliam@foxmail.com>
2022-03-28 23:51:58 +0800

    solve: 2257

--- questions/2257-medium-minusone/template.ts
+++ questions/2257-medium-minusone/template.ts
@@ -1,1 +1,11 @@
-type MinusOne<T extends number> = any
+type Nums2Arr<T extends number, R extends unknown[] = []> =
+  R['length'] extends T 
+    ? R 
+    : Nums2Arr<T, [...R, unknown]>
+
+type MinusOne<T extends number, R = Nums2Arr<T>> = 
+  R extends [infer _F, ...infer Rest]
+    ? Rest['length']
+    : never
+
+
