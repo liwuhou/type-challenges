cac9de95145212f978c8263c9e80108be452f07e
阿五 <hugewilliam@foxmail.com>
2022-03-31 23:38:51 +0800

    solve: 2693

--- questions/2693-medium-endswith/template.ts
+++ questions/2693-medium-endswith/template.ts
@@ -1,1 +1,7 @@
-type EndsWith<T extends string, U extends string> = any
+type EndsWith<T extends string, U extends string> =
+  T extends U
+    ? true
+    : T extends `${infer _F}${infer Rest}`
+      ? EndsWith<Rest, U>
+      : false
+
