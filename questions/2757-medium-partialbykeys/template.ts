46b330bf04e9dcf530fbb4e1144d00850a210de9
阿五 <hugewilliam@foxmail.com>
2022-04-01 20:55:23 +0800

    solve: 2757

--- questions/2757-medium-partialbykeys/template.ts
+++ questions/2757-medium-partialbykeys/template.ts
@@ -1,1 +1,10 @@
-type PartialByKeys<T , K> = any
+type MapObj<Obj extends Record<string, any>> = {
+  [Key in keyof Obj]: Obj[Key]
+}
+
+type PartialByKeys<
+  T extends Record<string, any>,
+  K extends string = never
+> = [K] extends [never] ? Partial<T> : MapObj<Pick<T, Exclude<keyof T, K>> & {
+  [Key in K as Key extends keyof T ? Key : never]?: Key extends keyof T ? T[Key] : never
+}>
--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,6 +1,6 @@
 import { Equal } from '@type-challenges/utils'
 
-export type Includes<T extends readonly any[], U> =
+export type Includes<T extends unknown[], U> =
   T extends [infer First, ...infer Rest]
     ? [First, U] extends [U, First]
       ? Equal<First, U>
