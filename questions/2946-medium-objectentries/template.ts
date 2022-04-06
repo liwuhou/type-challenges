76524e6bba7e5bb790f7312e24f743182670033a
阿五 <hugewilliam@foxmail.com>
2022-04-06 23:01:34 +0800

    solve: 2946

--- questions/2946-medium-objectentries/template.ts
+++ questions/2946-medium-objectentries/template.ts
@@ -1,1 +1,6 @@
-type ObjectEntries<T> = any
+type RemoveUndefined<T> = Exclude<T, undefined>
+
+type ObjectEntries<T extends Record<string, any>> = RemoveUndefined<{
+  [Key in keyof T]: [Key, RemoveUndefined<T[Key]>]
+}[keyof T]>
+
