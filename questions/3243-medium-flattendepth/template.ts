67391e4d587baf5fe4de1cb36c537a6f488eb1de
阿五 <hugewilliam@foxmail.com>
2022-04-11 23:22:48 +0800

    solve: 3243

--- questions/3243-medium-flattendepth/template.ts
+++ questions/3243-medium-flattendepth/template.ts
@@ -1,1 +1,9 @@
-type FlattenDepth = any
+type FlattenDepth<Arr extends any[], depth extends number = 1> = 
+  depth extends 0
+    ? Arr
+    : Arr extends [infer F, ...infer Rest]
+      ? F extends Array<any>
+        ? [...FlattenDepth<F, MinusOne<depth>>, ...FlattenDepth<Rest, depth>]
+        : [F, ...FlattenDepth<Rest, depth>]
+      : Arr
+
