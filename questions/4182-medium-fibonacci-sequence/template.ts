ec373496ec391a1783f83758f0af3bbe9a2ab095
阿五 <hugewilliam@foxmail.com>
2022-04-16 23:58:01 +0800

    solve: 4182

--- questions/4182-medium-fibonacci-sequence/template.ts
+++ questions/4182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,10 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<
+  T extends number,
+  Count extends unknown[] = [unknown],
+  F1 extends unknown[] = [],
+  F2 extends unknown[] = [unknown]
+> =
+Count['length'] extends T
+  ? F2['length']
+  : Fibonacci<T, [...Count, unknown], F2, [...F1, ...F2]>
+
