ea38fb7df46aad6c8d2a91c0b72b7acfaddc560a
阿五 <hugewilliam@foxmail.com>
2022-04-17 23:22:47 +0800

    solve: 4260

--- questions/4260-medium-nomiwase/template.ts
+++ questions/4260-medium-nomiwase/template.ts
@@ -1,1 +1,16 @@
-type AllCombinations<S> = any
+type Combinations<T extends string, Y extends string> =
+  T
+  | Y
+  | `${T}${Y}`
+  | `${Y}${T}`
+
+type UnionCombination<S extends string, Y extends string = S> =
+  S extends Y
+    ? Combinations<S, UnionCombination<Exclude<Y, S>>>
+    : never
+
+type AllCombinations<S extends string> = 
+  '' 
+  | UnionCombination<StringToUnion<S>>
+  
+
