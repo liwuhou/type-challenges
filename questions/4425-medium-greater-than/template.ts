19b5b9a7eecf9c869db5cca02ddfc058c19e6199
阿五 <hugewilliam@foxmail.com>
2022-04-18 22:34:03 +0800

    solve: 4425

--- questions/4425-medium-greater-than/template.ts
+++ questions/4425-medium-greater-than/template.ts
@@ -1,1 +1,8 @@
-type GreaterThan<T extends number, U extends number> = any
+type GreaterThan<T extends number, U extends number> =
+  T extends U
+    ? false
+    : U extends 0
+      ? true
+      : T extends 0
+        ? false
+        : GreaterThan<MinusOne<T>, MinusOne<U>>
