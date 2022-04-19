f29511297fdee4907506966b6651d5a127b1bed5
阿五 <hugewilliam@foxmail.com>
2022-04-19 22:52:16 +0800

    solve: 4471

--- questions/4471-medium-zip/template.ts
+++ questions/4471-medium-zip/template.ts
@@ -1,1 +1,11 @@
-type Zip = any
+type Zip<T extends unknown[], K extends unknown[], Res extends unknown[] = []> =
+  T['length'] extends 0
+    ? Res
+    : K['length'] extends 0
+      ? Res
+      : T extends [infer TF, ...infer TR]
+        ? K extends [infer KF, ...infer KR]
+          ? Zip<TR, KR, [...Res, [TF, KF]]>
+          : Res
+        : Res
+      
