55e2e944201c6af4c6928274bcd2c9ab1bc66944
阿五 <hugewilliam@foxmail.com>
2022-04-22 22:55:24 +0800

    solve: 4499

--- questions/4499-medium-chunk/template.ts
+++ questions/4499-medium-chunk/template.ts
@@ -1,1 +1,14 @@
-type Chunk = any
+type Chunk<
+  Arr extends unknown[],
+  Count extends number,
+  Result extends unknown[] = [],
+  Temp extends unknown[] = [],
+> = 
+  Arr extends [infer F, ...infer Rest]
+    ? Temp['length'] extends Count
+      ? Chunk<Rest, Count, [...Result, Temp], [F]>
+      : Chunk<Rest, Count, Result, [...Temp, F]>
+    : Temp['length'] extends 0
+      ? Result
+      : [...Result, Temp]
+
