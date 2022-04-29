1db2ff42342977c59bb72eb370f175cb6a9cfc63
阿五 <hugewilliam@foxmail.com>
2022-04-29 23:52:12 +0800

    solve: 5360

--- questions/4518-medium-fill/template.ts
+++ questions/4518-medium-fill/template.ts
@@ -2,7 +2,7 @@ type IsValidRange<
   T extends number,
   Start extends number,
   End extends number
-> = 
+> =
   GreaterThan<End, T> extends true
     ? T extends Start
       ? true
@@ -15,10 +15,9 @@ type Fill<
   Start extends number = 0,
   End extends number = T['length'],
   Res extends unknown[] = []
-> = 
+> =
   T extends [infer F, ...infer Rest]
     ? IsValidRange<Res['length'], Start, End> extends true
       ? Fill<Rest, N, Start, End, [...Res, N]>
       : Fill<Rest, N, Start, End, [...Res, F]>
     : Res
-
--- questions/5360-medium-unique/template.ts
+++ questions/5360-medium-unique/template.ts
@@ -1,1 +1,9 @@
-type Unique<T> = any
+import { Includes } from "../898-easy-includes/template";
+
+export type Unique<T extends unknown[], Res extends unknown[] = []> =
+  T extends [infer F, ...infer R]
+    ? Includes<Res, F> extends true
+      ? Unique<R, Res>
+      : Unique<R, [...Res, F]>
+    : Res
+
